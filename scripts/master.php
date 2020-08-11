1e543017700f2dfefa89fe5f06a0f686d1582539
Peter Markley <peter@malexmedia.net>
2020-08-11 19:04:41 -0400

    basic 'hello world' script for PHP port

---
+++ scripts/master.php
@@ -0,0 +1,33 @@
+#!/bin/php
+<?php
+/*
+#----------------------------------------------------------------------
+#                             Clockworth
+#          Robotic chiming wall clock made on Raspberry Pi.
+#           < https://github.com/petermarkley/clockworth >
+# 
+# Copyright 2018 by Peter Markley <peter@petermarkley.com>.
+# Distributed under the terms of the Lesser GNU General Public License.
+# 
+# This file is part of Clockworth.
+# 
+# Clockworth is free software: you can redistribute it and/or modify it
+# under the terms of the Lesser GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# 
+# Clockworth is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# Lesser GNU General Public License for more details.
+# 
+# You should have received a copy of the Lesser GNU General Public
+# License along with libmcmap.  If not, see
+# < http://www.gnu.org/licenses/ >.
+# 
+#----------------------------------------------------------------------
+*/
+
+echo "hello world\n";
+
+?>
