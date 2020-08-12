d6cdeb8a3b097f3881a7acc00c8fedd9075fee60
Peter Markley <peter@malexmedia.net>
2020-08-12 00:33:48 -0400

    adjust formatting of copyright notice

--- scripts/master.php
+++ scripts/master.php
@@ -1,32 +1,30 @@
 #!/bin/php
 <?php
-/*
-#----------------------------------------------------------------------
-#                             Clockworth
-#          Robotic chiming wall clock made on Raspberry Pi.
-#           < https://github.com/petermarkley/clockworth >
-# 
-# Copyright 2020 by Peter Markley <peter@petermarkley.com>.
-# Distributed under the terms of the Lesser GNU General Public License.
-# 
-# This file is part of Clockworth.
-# 
-# Clockworth is free software: you can redistribute it and/or modify it
-# under the terms of the Lesser GNU General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-# 
-# Clockworth is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# Lesser GNU General Public License for more details.
-# 
-# You should have received a copy of the Lesser GNU General Public
-# License along with libmcmap.  If not, see
-# < http://www.gnu.org/licenses/ >.
-# 
-#----------------------------------------------------------------------
-*/
+//----------------------------------------------------------------------
+//                             Clockworth
+//          Robotic chiming wall clock made on Raspberry Pi.
+//           < https://github.com/petermarkley/clockworth >
+// 
+// Copyright 2020 by Peter Markley <peter@petermarkley.com>.
+// Distributed under the terms of the Lesser GNU General Public License.
+// 
+// This file is part of Clockworth.
+// 
+// Clockworth is free software: you can redistribute it and/or modify it
+// under the terms of the Lesser GNU General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+// 
+// Clockworth is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// Lesser GNU General Public License for more details.
+// 
+// You should have received a copy of the Lesser GNU General Public
+// License along with libmcmap.  If not, see
+// < http://www.gnu.org/licenses/ >.
+// 
+//----------------------------------------------------------------------
 date_default_timezone_set(exec("timedatectl | grep -i \"time zone\" | cut -d':' -f2 | cut -d' ' -f2"));
 
 define("MAX_RECURSION_DEPTH", "100");
